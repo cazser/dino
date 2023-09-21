f36cf8eb2e9eeaee158b8bc5971b9e4f6a9b3716
cazser <2264712072@qq.com>
2023-09-21 20:08:37 +0800

    cloud indepent

--- js/Cloud/index.js
+++ js/Cloud/index.js
@@ -1,4 +1,6 @@
 import { getRandomNum } from "../utils";
+import { Runner } from "../Runner/index";
+import { IS_HIDPI } from "../shared_constant";
 //******************************************************************************
 /**
  * Cloud background item.
@@ -34,6 +36,71 @@ Cloud.config = {
   MIN_CLOUD_GAP: 100,
   MIN_SKY_LEVEL: 71,
   WIDTH: 46,
+};
+
+Cloud.prototype = {
+  /**
+   * Initialise the cloud. Sets the Cloud height.
+   */
+  init: function () {
+    this.yPos = getRandomNum(
+      Cloud.config.MAX_SKY_LEVEL,
+      Cloud.config.MIN_SKY_LEVEL
+    );
+    this.draw();
+  },
+
+  /**
+   * Draw the cloud.
+   */
+  draw: function () {
+    this.canvasCtx.save();
+    var sourceWidth = Cloud.config.WIDTH;
+    var sourceHeight = Cloud.config.HEIGHT;
+
+    if (IS_HIDPI) {
+      sourceWidth = sourceWidth * 2;
+      sourceHeight = sourceHeight * 2;
+    }
+
+    this.canvasCtx.drawImage(
+      Runner.imageSprite,
+      this.spritePos.x,
+      this.spritePos.y,
+      sourceWidth,
+      sourceHeight,
+      this.xPos,
+      this.yPos,
+      Cloud.config.WIDTH,
+      Cloud.config.HEIGHT
+    );
+
+    this.canvasCtx.restore();
+  },
+
+  /**
+   * Update the cloud position.
+   * @param {number} speed
+   */
+  update: function (speed) {
+    if (!this.remove) {
+      this.xPos -= Math.ceil(speed);
+      this.draw();
+
+      // Mark as removeable if no longer in the canvas.
+      if (!this.isVisible()) {
+        this.remove = true;
+      }
+    }
+  },
+
+  /**
+   * Check if the cloud is visible on the stage.
+   * @return {boolean}
+   */
+  isVisible: function () {
+    return this.xPos + Cloud.config.WIDTH > 0;
+  },
 };
 
 export {Cloud}
--- js/entry.js
+++ js/entry.js
@@ -1484,70 +1484,7 @@ DistanceMeter.prototype = {
   },
 };
 
-Cloud.prototype = {
-  /**
-   * Initialise the cloud. Sets the Cloud height.
-   */
-  init: function () {
-    this.yPos = getRandomNum(
-      Cloud.config.MAX_SKY_LEVEL,
-      Cloud.config.MIN_SKY_LEVEL
-    );
-    this.draw();
-  },
 
-  /**
-   * Draw the cloud.
-   */
-  draw: function () {
-    this.canvasCtx.save();
-    var sourceWidth = Cloud.config.WIDTH;
-    var sourceHeight = Cloud.config.HEIGHT;
-
-    if (IS_HIDPI) {
-      sourceWidth = sourceWidth * 2;
-      sourceHeight = sourceHeight * 2;
-    }
-
-    this.canvasCtx.drawImage(
-      Runner.imageSprite,
-      this.spritePos.x,
-      this.spritePos.y,
-      sourceWidth,
-      sourceHeight,
-      this.xPos,
-      this.yPos,
-      Cloud.config.WIDTH,
-      Cloud.config.HEIGHT
-    );
-
-    this.canvasCtx.restore();
-  },
-
-  /**
-   * Update the cloud position.
-   * @param {number} speed
-   */
-  update: function (speed) {
-    if (!this.remove) {
-      this.xPos -= Math.ceil(speed);
-      this.draw();
-
-      // Mark as removeable if no longer in the canvas.
-      if (!this.isVisible()) {
-        this.remove = true;
-      }
-    }
-  },
-
-  /**
-   * Check if the cloud is visible on the stage.
-   * @return {boolean}
-   */
-  isVisible: function () {
-    return this.xPos + Cloud.config.WIDTH > 0;
-  },
-};
 
 
 
