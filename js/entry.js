9f9fe191cf6283f9878027ea6900e8d66292c552
cazser <2264712072@qq.com>
2023-09-21 15:32:26 +0800

    seprate game over panel

--- js/entry.js
+++ js/entry.js
@@ -1,3 +1,4 @@
+import { GameOverPanel } from "./GameOverPanel";
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -1112,113 +1113,7 @@ function getTimeStamp() {
   return IS_IOS ? new Date().getTime() : performance.now();
 }
 
-//******************************************************************************
 
-/**
- * Game over panel.
- * @param {!HTMLCanvasElement} canvas
- * @param {Object} textImgPos
- * @param {Object} restartImgPos
- * @param {!Object} dimensions Canvas dimensions.
- * @constructor
- */
-function GameOverPanel(canvas, textImgPos, restartImgPos, dimensions) {
-  this.canvas = canvas;
-  this.canvasCtx = canvas.getContext("2d");
-  this.canvasDimensions = dimensions;
-  this.textImgPos = textImgPos;
-  this.restartImgPos = restartImgPos;
-  this.draw();
-}
-
-/**
- * Dimensions used in the panel.
- * @enum {number}
- */
-GameOverPanel.dimensions = {
-  TEXT_X: 0,
-  TEXT_Y: 13,
-  TEXT_WIDTH: 191,
-  TEXT_HEIGHT: 11,
-  RESTART_WIDTH: 36,
-  RESTART_HEIGHT: 32,
-};
-
-GameOverPanel.prototype = {
-  /**
-   * Update the panel dimensions.
-   * @param {number} width New canvas width.
-   * @param {number} opt_height Optional new canvas height.
-   */
-  updateDimensions: function (width, opt_height) {
-    this.canvasDimensions.WIDTH = width;
-    if (opt_height) {
-      this.canvasDimensions.HEIGHT = opt_height;
-    }
-  },
-  /**
-   * Draw the panel.
-   */
-  draw: function () {
-    var dimensions = GameOverPanel.dimensions;
-
-    var centerX = this.canvasDimensions.WIDTH / 2;
-
-    // Game over text.
-    var textSourceX = dimensions.TEXT_X;
-    var textSourceY = dimensions.TEXT_Y;
-    var textSourceWidth = dimensions.TEXT_WIDTH;
-    var textSourceHeight = dimensions.TEXT_HEIGHT;
-
-    var textTargetX = Math.round(centerX - dimensions.TEXT_WIDTH / 2);
-    var textTargetY = Math.round((this.canvasDimensions.HEIGHT - 25) / 3);
-    var textTargetWidth = dimensions.TEXT_WIDTH;
-    var textTargetHeight = dimensions.TEXT_HEIGHT;
-
-    var restartSourceWidth = dimensions.RESTART_WIDTH;
-    var restartSourceHeight = dimensions.RESTART_HEIGHT;
-    var restartTargetX = centerX - dimensions.RESTART_WIDTH / 2;
-    var restartTargetY = this.canvasDimensions.HEIGHT / 2;
-
-    if (IS_HIDPI) {
-      textSourceY *= 2;
-      textSourceX *= 2;
-      textSourceWidth *= 2;
-      textSourceHeight *= 2;
-      restartSourceWidth *= 2;
-      restartSourceHeight *= 2;
-    }
-
-    textSourceX += this.textImgPos.x;
-    textSourceY += this.textImgPos.y;
-
-    // Game over text from sprite.
-    this.canvasCtx.drawImage(
-      Runner.imageSprite,
-      textSourceX,
-      textSourceY,
-      textSourceWidth,
-      textSourceHeight,
-      textTargetX,
-      textTargetY,
-      textTargetWidth,
-      textTargetHeight
-    );
-
-    // Restart button.
-    this.canvasCtx.drawImage(
-      Runner.imageSprite,
-      this.restartImgPos.x,
-      this.restartImgPos.y,
-      restartSourceWidth,
-      restartSourceHeight,
-      restartTargetX,
-      restartTargetY,
-      dimensions.RESTART_WIDTH,
-      dimensions.RESTART_HEIGHT
-    );
-  },
-}
 
 //******************************************************************************
 
