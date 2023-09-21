073397e2c321897d39f70c99c8b08e39c227e0d4
cazser <2264712072@qq.com>
2023-09-21 19:14:13 +0800

    seprate Obstacle

---
+++ js/Obstacle/index.js
@@ -0,0 +1,49 @@
+//******************************************************************************
+import { getRandomNum } from "../utils";
+/**
+ * Obstacle.
+ * @param {HTMLCanvasCtx} canvasCtx
+ * @param {Obstacle.type} type
+ * @param {Object} spritePos Obstacle position in sprite.
+ * @param {Object} dimensions
+ * @param {number} gapCoefficient Mutipler in determining the gap.
+ * @param {number} speed
+ * @param {number} opt_xOffset
+ */
+function Obstacle(
+  canvasCtx,
+  type,
+  spriteImgPos,
+  dimensions,
+  gapCoefficient,
+  speed,
+  opt_xOffset
+) {
+  this.canvasCtx = canvasCtx;
+  this.spritePos = spriteImgPos;
+  this.typeConfig = type;
+  this.gapCoefficient = gapCoefficient;
+  this.size = getRandomNum(1, Obstacle.MAX_OBSTACLE_LENGTH);
+  this.dimensions = dimensions;
+  this.remove = false;
+  this.xPos = dimensions.WIDTH + (opt_xOffset || 0);
+  this.yPos = 0;
+  this.width = 0;
+  this.collisionBoxes = [];
+  this.gap = 0;
+  this.speedOffset = 0;
+
+  // For animated obstacles.
+  this.currentFrame = 0;
+  this.timer = 0;
+
+  this.init(speed);
+}
+
+/**
+ * Coefficient for calculating the maximum gap.
+ * @const
+ */
+Obstacle.MAX_GAP_COEFFICIENT = 1.5;
+
+export {Obstacle}
--- js/entry.js
+++ js/entry.js
@@ -17,6 +17,7 @@ import { Runner_events } from "./Runner/events";
 import { Runner_keycodes } from "./Runner/keycodes";
 import { Runner_updateCanvasScaling } from "./Runner/updateCanvasScaling";
 import { Runner_handleEvent } from "./Runner/handleEvent";
+import { Obstacle } from "./Obstacle/index";
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -968,54 +969,6 @@ function boxCompare(tRexBox, obstacleBox) {
   return crashed;
 }
 
-
-//******************************************************************************
-
-/**
- * Obstacle.
- * @param {HTMLCanvasCtx} canvasCtx
- * @param {Obstacle.type} type
- * @param {Object} spritePos Obstacle position in sprite.
- * @param {Object} dimensions
- * @param {number} gapCoefficient Mutipler in determining the gap.
- * @param {number} speed
- * @param {number} opt_xOffset
- */
-function Obstacle(
-  canvasCtx,
-  type,
-  spriteImgPos,
-  dimensions,
-  gapCoefficient,
-  speed,
-  opt_xOffset
-) {
-  this.canvasCtx = canvasCtx;
-  this.spritePos = spriteImgPos;
-  this.typeConfig = type;
-  this.gapCoefficient = gapCoefficient;
-  this.size = getRandomNum(1, Obstacle.MAX_OBSTACLE_LENGTH);
-  this.dimensions = dimensions;
-  this.remove = false;
-  this.xPos = dimensions.WIDTH + (opt_xOffset || 0);
-  this.yPos = 0;
-  this.width = 0;
-  this.collisionBoxes = [];
-  this.gap = 0;
-  this.speedOffset = 0;
-
-  // For animated obstacles.
-  this.currentFrame = 0;
-  this.timer = 0;
-
-  this.init(speed);
-}
-
-/**
- * Coefficient for calculating the maximum gap.
- * @const
- */
-Obstacle.MAX_GAP_COEFFICIENT = 1.5;
 
 /**
  * Maximum obstacle grouping count.
