a680761cfdb5ff4043c3f6b5a16154364452e079
cazser <2264712072@qq.com>
2023-09-21 17:46:56 +0800

    seprate DistanceMeter

---
+++ js/DistanceMeter/index.js
@@ -0,0 +1,35 @@
+//******************************************************************************
+
+/**
+ * Handles displaying the distance meter.
+ * @param {!HTMLCanvasElement} canvas
+ * @param {Object} spritePos Image position in sprite.
+ * @param {number} canvasWidth
+ * @constructor
+ */
+function DistanceMeter(canvas, spritePos, canvasWidth) {
+  this.canvas = canvas;
+  this.canvasCtx = canvas.getContext("2d");
+  this.image = Runner.imageSprite;
+  this.spritePos = spritePos;
+  this.x = 0;
+  this.y = 5;
+
+  this.currentDistance = 0;
+  this.maxScore = 0;
+  this.highScore = 0;
+  this.container = null;
+
+  this.digits = [];
+  this.acheivement = false;
+  this.defaultString = "";
+  this.flashTimer = 0;
+  this.flashIterations = 0;
+  this.invertTrigger = false;
+
+  this.config = DistanceMeter.config;
+  this.maxScoreUnits = this.config.MAX_DISTANCE_UNITS;
+  this.init(canvasWidth);
+}
+
+export {DistanceMeter}
--- js/entry.js
+++ js/entry.js
@@ -9,6 +9,7 @@ import { CollisionBox } from "./CollisionBox/index";
 import { NightMode } from "./NightMode/index";
 import { DEFAULT_WIDTH, FPS, IS_HIDPI, IS_IOS, IS_MOBILE, IS_TOUCH_ENABLED } from "./shared_constant";
 import { Runner_startGame } from "./Runner/startGame";
+import { DistanceMeter } from "./DistanceMeter/index";
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -1742,39 +1743,7 @@ Trex.prototype = {
   },
 };
 
-//******************************************************************************
 
-/**
- * Handles displaying the distance meter.
- * @param {!HTMLCanvasElement} canvas
- * @param {Object} spritePos Image position in sprite.
- * @param {number} canvasWidth
- * @constructor
- */
-function DistanceMeter(canvas, spritePos, canvasWidth) {
-  this.canvas = canvas;
-  this.canvasCtx = canvas.getContext("2d");
-  this.image = Runner.imageSprite;
-  this.spritePos = spritePos;
-  this.x = 0;
-  this.y = 5;
-
-  this.currentDistance = 0;
-  this.maxScore = 0;
-  this.highScore = 0;
-  this.container = null;
-
-  this.digits = [];
-  this.acheivement = false;
-  this.defaultString = "";
-  this.flashTimer = 0;
-  this.flashIterations = 0;
-  this.invertTrigger = false;
-
-  this.config = DistanceMeter.config;
-  this.maxScoreUnits = this.config.MAX_DISTANCE_UNITS;
-  this.init(canvasWidth);
-}
 
 /**
  * @enum {number}
