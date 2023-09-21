1ec17550824d871a925faca32967ece7755e6e71
cazser <2264712072@qq.com>
2023-09-21 16:51:04 +0800

    seprate NightMode

---
+++ js/NightMode/index.js
@@ -0,0 +1,36 @@
+//******************************************************************************
+
+/**
+ * Nightmode shows a moon and stars on the horizon.
+ */
+function NightMode(canvas, spritePos, containerWidth) {
+  this.spritePos = spritePos;
+  this.canvas = canvas;
+  this.canvasCtx = canvas.getContext("2d");
+  this.xPos = containerWidth - 50;
+  this.yPos = 30;
+  this.currentPhase = 0;
+  this.opacity = 0;
+  this.containerWidth = containerWidth;
+  this.stars = [];
+  this.drawStars = false;
+  this.placeStars();
+}
+
+/**
+ * @enum {number}
+ */
+NightMode.config = {
+  FADE_SPEED: 0.035,
+  HEIGHT: 40,
+  MOON_SPEED: 0.25,
+  NUM_STARS: 2,
+  STAR_SIZE: 9,
+  STAR_SPEED: 0.3,
+  STAR_MAX_Y: 70,
+  WIDTH: 20,
+};
+
+NightMode.phases = [140, 120, 100, 60, 40, 20, 0];
+
+export {NightMode}
--- js/entry.js
+++ js/entry.js
@@ -6,6 +6,7 @@ import { Horizon } from "./Horizon/index";
 import { HorizonLine } from "./HorizonLine/index";
 import { Cloud } from "./Cloud/index";
 import { CollisionBox } from "./CollisionBox/index";
+import { NightMode } from "./NightMode/index";
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -2116,40 +2117,6 @@ Cloud.prototype = {
   },
 };
 
-//******************************************************************************
-
-/**
- * Nightmode shows a moon and stars on the horizon.
- */
-function NightMode(canvas, spritePos, containerWidth) {
-  this.spritePos = spritePos;
-  this.canvas = canvas;
-  this.canvasCtx = canvas.getContext("2d");
-  this.xPos = containerWidth - 50;
-  this.yPos = 30;
-  this.currentPhase = 0;
-  this.opacity = 0;
-  this.containerWidth = containerWidth;
-  this.stars = [];
-  this.drawStars = false;
-  this.placeStars();
-}
-
-/**
- * @enum {number}
- */
-NightMode.config = {
-  FADE_SPEED: 0.035,
-  HEIGHT: 40,
-  MOON_SPEED: 0.25,
-  NUM_STARS: 2,
-  STAR_SIZE: 9,
-  STAR_SPEED: 0.3,
-  STAR_MAX_Y: 70,
-  WIDTH: 20,
-};
-
-NightMode.phases = [140, 120, 100, 60, 40, 20, 0];
 
 NightMode.prototype = {
   /**
