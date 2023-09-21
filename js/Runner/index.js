0936621431f87e077aad19dec1716dff71bac333
cazser <2264712072@qq.com>
2023-09-21 19:28:38 +0800

    seprate Runner

---
+++ js/Runner/index.js
@@ -0,0 +1,72 @@
+import { FPS } from "../shared_constant";
+("use strict");
+/**
+ * T-Rex runner.
+ * @param {string} outerContainerId Outer containing element id.
+ * @param {Object} opt_config
+ * @constructor
+ * @export
+ */
+
+function Runner(outerContainerId, opt_config) {
+  // 单例
+  if (Runner.instance_) {
+    return Runner.instance_;
+  }
+  Runner.instance_ = this;
+
+  this.outerContainerEl = document.querySelector(outerContainerId);
+  this.containerEl = null;
+  this.snackbarEl = null;
+  this.detailsButton = this.outerContainerEl.querySelector("#details-button");
+
+  this.config = opt_config || Runner.config;
+
+  this.dimensions = Runner.defaultDimensions;
+
+  this.canvas = null;
+  this.canvasCtx = null;
+
+  this.tRex = null;
+
+  this.distanceMeter = null;
+  this.distanceRan = 0;
+
+  this.highestScore = 0;
+
+  this.time = 0;
+  this.runningTime = 0;
+  this.msPerFrame = 1000 / FPS;
+  this.currentSpeed = this.config.SPEED;
+
+  this.obstacles = [];
+
+  this.activated = false; // Whether the easter egg has been activated.
+  this.playing = false; // Whether the game is currently in play state.
+  this.crashed = false;
+  this.paused = false;
+  this.inverted = false;
+  this.invertTimer = 0;
+  this.resizeTimerId_ = null;
+
+  this.playCount = 0;
+
+  // Sound FX.
+  this.audioBuffer = null;
+  this.soundFx = {};
+
+  // Global web audio context for playing sounds.
+  this.audioContext = null;
+
+  // Images.
+  this.images = {};
+  this.imagesLoaded = 0;
+
+  if (this.isDisabled()) {
+    this.setupDisabledRunner();
+  } else {
+    this.loadImages();
+  }
+}
+
+export {Runner}
