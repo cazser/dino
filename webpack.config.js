2e8274186b78582b4ae1c55b98e71aa6358a0530
cazser <2264712072@qq.com>
2023-09-22 14:16:11 +0800

    rename js src

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/entry.js',
+  entry: './src/entry.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
