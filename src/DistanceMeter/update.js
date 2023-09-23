3256fc35e0cc7d6fb9c3df2c773efed4d9343209
cazser <2264712072@qq.com>
2023-09-23 15:39:59 +0800

    Distance Meter update

---
+++ src/DistanceMeter/update.js
@@ -0,0 +1,64 @@
+const DistanceMeter_update=function (deltaTime, distance) {
+    var paint = true;
+    var playSound = false;
+
+    if (!this.acheivement) {
+      distance = this.getActualDistance(distance);
+      // Score has gone beyond the initial digit count.
+      if (
+        distance > this.maxScore &&
+        this.maxScoreUnits == this.config.MAX_DISTANCE_UNITS
+      ) {
+        this.maxScoreUnits++;
+        this.maxScore = parseInt(this.maxScore + "9");
+      } else {
+        this.distance = 0;
+      }
+
+      if (distance > 0) {
+        // Acheivement unlocked
+        if (distance % this.config.ACHIEVEMENT_DISTANCE == 0) {
+          // Flash score and play sound.
+          this.acheivement = true;
+          this.flashTimer = 0;
+          playSound = true;
+        }
+
+        // Create a string representation of the distance with leading 0.
+        var distanceStr = (this.defaultString + distance).substr(
+          -this.maxScoreUnits
+        );
+        this.digits = distanceStr.split("");
+      } else {
+        this.digits = this.defaultString.split("");
+      }
+    } else {
+      // Control flashing of the score on reaching acheivement.
+      if (this.flashIterations <= this.config.FLASH_ITERATIONS) {
+        this.flashTimer += deltaTime;
+
+        if (this.flashTimer < this.config.FLASH_DURATION) {
+          paint = false;
+        } else if (this.flashTimer > this.config.FLASH_DURATION * 2) {
+          this.flashTimer = 0;
+          this.flashIterations++;
+        }
+      } else {
+        this.acheivement = false;
+        this.flashIterations = 0;
+        this.flashTimer = 0;
+      }
+    }
+
+    // Draw the digits if not flashing.
+    if (paint) {
+      for (var i = this.digits.length - 1; i >= 0; i--) {
+        this.draw(i, parseInt(this.digits[i]));
+      }
+    }
+
+    this.drawHighScore();
+    return playSound;
+  }
+
+  export {DistanceMeter_update}
