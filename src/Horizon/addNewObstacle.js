e6dadb149157779de22cd2ae3cf212f7dac9d884
cazser <2264712072@qq.com>
2023-09-23 15:49:07 +0800

    +1

---
+++ src/Horizon/addNewObstacle.js
@@ -0,0 +1,39 @@
+import { getRandomNum } from "../utils";
+import { Obstacle } from "../Obstacle/index";
+import { IS_MOBILE } from "../shared_constant";
+const Horizon_addNewObstacle = function (currentSpeed) {
+    var obstacleTypeIndex = getRandomNum(0, Obstacle.types.length - 1);
+    var obstacleType = Obstacle.types[obstacleTypeIndex];
+
+    // Check for multiples of the same type of obstacle.
+    // Also check obstacle is available at current speed.
+    if (
+      this.duplicateObstacleCheck(obstacleType.type) ||
+      currentSpeed < obstacleType.minSpeed
+    ) {
+      this.addNewObstacle(currentSpeed);
+    } else {
+      var obstacleSpritePos = this.spritePos[obstacleType.type];
+
+      this.obstacles.push(
+        new Obstacle(
+          this.canvasCtx,
+          obstacleType,
+          obstacleSpritePos,
+          this.dimensions,
+          this.gapCoefficient,
+          currentSpeed,
+          obstacleType.width
+        )
+      );
+
+      this.obstacleHistory.unshift(obstacleType.type);
+
+      if (this.obstacleHistory.length > 1) {
+        this.obstacleHistory.splice(Runner.config.MAX_OBSTACLE_DUPLICATION);
+      }
+    }
+  }
+
+
+export {Horizon_addNewObstacle}
--- src/Horizon/index.js
+++ src/Horizon/index.js
@@ -3,6 +3,7 @@ import { getRandomNum } from "../utils";
 import { Obstacle } from "../Obstacle/index";
 import { Cloud } from "../Cloud/index";
 import { FPS } from "../shared_constant";
+import { Horizon_addNewObstacle } from "./addNewObstacle";
 /**
  * Horizon background class.
  * @param {HTMLCanvasElement} canvas
@@ -152,40 +153,7 @@ Horizon.prototype = {
    * Add a new obstacle.
    * @param {number} currentSpeed
    */
-  addNewObstacle: function (currentSpeed) {
-    var obstacleTypeIndex = getRandomNum(0, Obstacle.types.length - 1);
-    var obstacleType = Obstacle.types[obstacleTypeIndex];
-
-    // Check for multiples of the same type of obstacle.
-    // Also check obstacle is available at current speed.
-    if (
-      this.duplicateObstacleCheck(obstacleType.type) ||
-      currentSpeed < obstacleType.minSpeed
-    ) {
-      this.addNewObstacle(currentSpeed);
-    } else {
-      var obstacleSpritePos = this.spritePos[obstacleType.type];
-
-      this.obstacles.push(
-        new Obstacle(
-          this.canvasCtx,
-          obstacleType,
-          obstacleSpritePos,
-          this.dimensions,
-          this.gapCoefficient,
-          currentSpeed,
-          obstacleType.width
-        )
-      );
-
-      this.obstacleHistory.unshift(obstacleType.type);
-
-      if (this.obstacleHistory.length > 1) {
-        this.obstacleHistory.splice(Runner.config.MAX_OBSTACLE_DUPLICATION);
-      }
-    }
-  },
-
+  addNewObstacle:Horizon_addNewObstacle, 
   /**
    * Returns whether the previous two obstacles are the same as the next one.
    * Maximum duplication is set in config value MAX_OBSTACLE_DUPLICATION.
