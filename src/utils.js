ee49ee5df5b81687ad2149df33cce5b431ddf2e2
cazser <2264712072@qq.com>
2023-09-23 09:26:39 +0800

    +1

--- src/utils.js
+++ src/utils.js
@@ -179,5 +179,28 @@ function drawCollisionBoxes(canvasCtx, tRexBox, obstacleBox) {
   );
   canvasCtx.restore();
 }
+
+
+/**
+ * Create canvas element.
+ * @param {HTMLElement} container Element to append canvas to.
+ * @param {number} width
+ * @param {number} height
+ * @param {string} opt_classname
+ * @return {HTMLCanvasElement}
+ */
+function createCanvas(container, width, height, opt_classname) {
+  var canvas = document.createElement("canvas");
+  canvas.className = opt_classname
+    ? Runner.classes.CANVAS + " " + opt_classname
+    : Runner.classes.CANVAS;
+  canvas.width = width;
+  canvas.height = height;
+  container.appendChild(canvas);
+
+  return canvas;
+}
+
+
 export{ getRandomNum, createCanvas , boxCompare, getTimeStamp, vibrate, decodeBase64ToArrayBuffer,
 checkForCollision, createAdjustedCollisionBox, drawCollisionBoxes}
